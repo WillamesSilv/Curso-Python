6886c02fdb0b7fc947580e4df0b642243bef2527
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-04-15 14:32:01 -0300

    resolvido

--- desafio058.py
+++ desafio058.py
@@ -4,12 +4,25 @@ cont = 0
 novopalpite = 0
 computador = randint(0, 10)
 print('De 0 á 10, tente adivinhar em qual número estou pensando.')
-print(computador)
-sleep(5)
+sleep(3)
 usuario = int(input('Qual é o número que pensei? '))
 while usuario != computador:
     usuario = int(input('ERROU! Continue tentando: '))
     cont += 1
 if usuario == computador:
     print('PARABÉNS VOCÊ CONSEGUIU ACERTAR!')
-print('Você usou {} tentativas.'.format(cont))
+print('Você usou {} tentativas.'.format(cont + 1))
+'''programa do guanabara
+acertou = false
+palpites = 0
+while not acertou:
+    jogador = int(input(Qual pe o se palpite? ))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print(Mais... Tente mias uma vez.)
+        elif jogador > computador:
+            print(Menos... Tente mais uma vez.)
+print(Acertou com {} tentativas. Parabéns!.format(palpites)) '''
