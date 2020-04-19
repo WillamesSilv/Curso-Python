a85ec3a34a51aecc7b3bf0c0dc839bdaceca7ceb
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-04-19 15:25:27 -0300

    resolvido

--- desafio060.py
+++ desafio060.py
@@ -1,5 +1,14 @@
+'''from math import factorial
 numero = int(input('Digite um número para saber seu fatorial: '))
-f = 0
-while f == 1:
-    numero = numero % 2 == 0
-    print(numero)
+f = factorial(numero)
+print('O fatorial de {} é {}.'.format(numero, f))'''
+n = int(input('Digite um número: '))
+c = n
+f = 1 # quando somamos o valor é 0, mas quando é multiplicação o valor é 1.
+print('Calculando o {}! = '.format(n), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')#Depois do = ja poderia colocar a importação do factorial
+    f *= c
+    c -= 1
+print('{}.'.format(f))
