f77db3f70a41cce51aae99ac2d906b8064eb8f1c
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-04-26 20:55:32 -0300

    laço for

--- desafio051.py
+++ desafio051.py
@@ -1,6 +1,6 @@
 termo = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razão: '))
-decimo = termo + (10 - 1) * razao #regra da PA para o decimo termo, se fosseo vigesimo colaria 20 no lugar do (10 - 1)
+decimo = termo + (10 - 1) * razao #regra da PA para o decimo termo, se fosse o vigesimo colaria 20 no lugar do (10 - 1)
 for c in range(termo, decimo+razao, razao):
     print('{}'.format(c), end=' - ')
 print('fim')
