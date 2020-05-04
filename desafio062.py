97291a91493c676f3471fceebe355ad64876b9e6
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-04 14:45:16 -0300

    resolvido

--- desafio062.py
+++ desafio062.py
@@ -2,8 +2,14 @@ primeiro = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razão: '))
 termo = primeiro
 cont = 1
-while cont <= 10:
-    print('{} -'.format(termo), end='')
-    termo += razao
-    cont += 1
-print('FIM')
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print('{} -'.format(termo), end=' ')
+        termo += razao
+        cont += 1
+    print('Pausa')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos.'.format(total))
