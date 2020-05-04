27c02e765837e89dca32cdc09a998d6ffb458169
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-04 11:17:33 -0300

    resolvido

--- desafio061.py
+++ desafio061.py
@@ -1,6 +1,15 @@
 termo = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razão: '))
-decimo = termo + (10 - 1) * razao #regra da PA para o decimo termo, se fosse o vigesimo colaria 20 no lugar do (10 - 1)
-for c in range(termo, decimo+razao, razao):
-    print('{}'.format(c), end=' - ')
+decimo = termo - razao
+c = 0
+while c < 10:
+    c += 1
+    decimo += razao
+    print('{}'.format(decimo), end=' - ')
 print('fim')
+'''Resolução do Guanabara, ele usou um termo = primeiro e um cont = 1
+while cont <= 10:
+    print('{} -'.format(termo), end='')
+    termo += razao
+    cont += 1
+print('FIM')'''
