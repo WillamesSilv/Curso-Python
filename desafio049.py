a231654acc058a8e9576338e0e3f988b6c115d90
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-08 14:04:32 -0300

    Update desafio049.py

--- desafio049.py
+++ desafio049.py
@@ -1,3 +1,7 @@
+from time import sleep
 tabuada = int(input('Digite um número para ver a sua tabuada: '))
+print('=-=' * 5)
 for c in range(0, 11):
-    print('{} x {:2} = {}'.format(tabuada, c, tabuada * c))
+    sleep(1)
+    print('\033[32m{} x {:2} = {}'.format(tabuada, c, tabuada * c))
+print('\033[m=-=' * 5)
