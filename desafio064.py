ed8b32e1ba89624b6d49a9374352f1c7128765c8
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-08 22:15:01 -0300

    resolvido

--- desafio064.py
+++ desafio064.py
@@ -1,6 +1,8 @@
 n = 1
 cont = 0
+soma = 0
 while n != 999:
     n = int(input('Digite um número: '))
     cont += 1
-print('Você digitou {} números.'.format(cont))
+    soma += n
+print('Você digitou {} números e a soma destes é {} desconsiderando o 999.'.format(cont, soma - 999))
