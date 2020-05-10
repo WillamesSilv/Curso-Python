d9cfeed687529ab1e1a620d5bba6097297825dd8
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-10 15:16:05 -0300

    introdução

---
+++ desafio065.py
@@ -0,0 +1,5 @@
+n = int(input('digite um número: '))
+r = str(input('Quer continuar? [S/N]: ')).upper()
+while r != 'N':
+    n = int(input('Digite um número: '))
+    r = str(input('Quer continuar? [S/N]: ')).upper()
