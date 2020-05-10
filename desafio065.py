f97b990e406980cbc174edb6436089fc07eb51a3
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-10 17:35:31 -0300

    Resolvido

--- desafio065.py
+++ desafio065.py
@@ -1,9 +1,16 @@
-n = soma = cont = med = 0
+n = soma = cont = med = maior = menor =0
 r = 'S'
 while r == 'S':
     n = int(input('Digite um número: '))
     r = str(input('Quer continuar? [S/N]: ')).upper()
+    if cont == 0:
+        maior = menor = n
+    if n > maior:
+        maior = n
+    elif n < menor:
+        menor = n
     cont += 1
     soma += n
     med = soma / cont
 print('Você digitou {} números, a soma entre eles é {}.\nA média é {:.1f}.'.format(cont, soma, med))
+print('O {} é o MAIOR. O {} é o MENOR.'.format(maior, menor))
