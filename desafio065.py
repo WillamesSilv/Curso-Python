9eb63de5c5c0ac28430166a651f97d2c3af9c775
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-10 16:42:31 -0300

    add, soma e  média

--- desafio065.py
+++ desafio065.py
@@ -1,5 +1,10 @@
+n = soma = cont = 0
 n = int(input('digite um número: '))
 r = str(input('Quer continuar? [S/N]: ')).upper()
 while r != 'N':
     n = int(input('Digite um número: '))
     r = str(input('Quer continuar? [S/N]: ')).upper()
+    cont += 1
+    soma += n
+    med = soma / cont
+print('Você digitou {} números, a soma entre eles é {}.\nA média é {:.1f}.'.format(cont, soma, med))
