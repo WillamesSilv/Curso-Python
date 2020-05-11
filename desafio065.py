c6b43ceb73dc1fc3ce037bbfbded2837e0211573
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-11 10:12:53 -0300

    Update desafio065.py

--- desafio065.py
+++ desafio065.py
@@ -12,5 +12,5 @@ while r == 'S':#Podemos usar tbm o in 'Ss'. o in é em.
     cont += 1
     soma += n
 med = soma / cont #Guanabara usou a média fora do laço, não se precisa calcular a media cada vez que digitar um núm
-print('Você digitou \033[32m{} números, \033[33ma soma entre eles é {}.\n\033[34mA média é {:.1f}.'.format(cont, soma, med))
+print('Você digitou:\n\033[32m{} Números, \033[33ma soma entre eles é {}.\n\033[34mA média é {:.1f}.'.format(cont, soma, med))
 print('\033[35mO {} é o MAIOR.\n\033[36mO {} é o MENOR.'.format(maior, menor))
