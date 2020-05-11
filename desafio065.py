28af8a52e76111a80babcce57ca6a0dda1d150c8
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-11 10:04:39 -0300

    Update desafio065.py

--- desafio065.py
+++ desafio065.py
@@ -3,7 +3,7 @@ r = 'S'
 while r == 'S':#Podemos usar tbm o in 'Ss'. o in é em.
     n = int(input('Digite um número: '))
     r = str(input('Quer continuar? [S/N]: ')).upper()#strip() [0], para aceitar apenas a primeira letra
-    if cont == 1:#Raciocinio logico, se um unico numero foi digitado, entao ele é o maior e o menor até o momento.
+    if cont == 0:#Raciocinio logico, se um unico numero foi digitado, entao ele é o maior e o menor até o momento.
         maior = menor = n
     if n > maior:
         maior = n
@@ -12,5 +12,5 @@ while r == 'S':#Podemos usar tbm o in 'Ss'. o in é em.
     cont += 1
     soma += n
 med = soma / cont #Guanabara usou a média fora do laço, não se precisa calcular a media cada vez que digitar um núm
-print('Você digitou {} números, a soma entre eles é {}.\nA média é {:.1f}.'.format(cont, soma, med))
-print('O {} é o MAIOR. O {} é o MENOR.'.format(maior, menor))
+print('Você digitou \033[32m{} números, \033[33ma soma entre eles é {}.\n\033[34mA média é {:.1f}.'.format(cont, soma, med))
+print('\033[35mO {} é o MAIOR.\033[36m O {} é o MENOR.'.format(maior, menor))
