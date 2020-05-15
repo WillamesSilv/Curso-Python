1edac7e92abb11c0975d0614ba8db50423c55d49
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-15 00:05:22 -0300

    Resolvido

--- desafio067.py
+++ desafio067.py
@@ -1,8 +1,10 @@
 tb = 0
 while True:
     tb = int(input('Quer ver a tabuada de qual número? '))
+    print('\033[33m='*35)
     if tb < 0:
         break
     for c in range(0, 11):
-        print(f'{tb} x {c} = {tb * c}')
-print('Programa de Tabuada Encerrado!')
+        print(f'\033[34m{tb} x {c} = {tb * c}\033[m')
+    print('\033[33m=\033[m'*35)
+print('\033[31mPROGRAMA DE TABUADA ENCERRADO.')
