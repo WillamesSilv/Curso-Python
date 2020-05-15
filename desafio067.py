5d5ebcf20132ae4707eee9baf9e107568ffedb65
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-14 23:45:32 -0300

    Update desafio067.py

--- desafio067.py
+++ desafio067.py
@@ -1,8 +1,10 @@
 tb = 0
 while True:
     tb = int(input('Quer ver a tabuada de qual número? '))
-    for c in range (0, 11):
-        print(f'{tb} x {c} = {tb * c}')
     if tb == -1:
         break
+
+    for c in range(0, 11):
+        print(f'{tb} x {c} = {tb * c}')
+
 print('Fim')
