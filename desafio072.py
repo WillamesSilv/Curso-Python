eb9fb4877784609ca7a6a1ce6270eea21d8ab947
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-22 23:18:21 -0300

    Resolvido

--- desafio072.py
+++ desafio072.py
@@ -1,6 +1,6 @@
 numero = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 n = int(input('Digite um número entre 0 e 20: '))
-if n not in numero:
+while n < 0 or n > 20:
     n = int(input('Tente novamente. Digite um número entre 0 e 20: '))
 print(f'Você digitou o número {numero[n]}.')
 
