f5851a3b32e53343c6bb28ea4b2a272b86f65c85
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-26 22:47:45 -0300

    Resolvido

--- desafio073.py
+++ desafio073.py
@@ -4,4 +4,5 @@ times = ('Palmeiras', 'Flamengo', 'Internacional', 'Grêmio', 'São-Paulo', 'Atl
 print(f'Classificação do Brasileirão 2018: {times}')
 print(f'Os 5 PRIMEIROS são: {times[:5]}')
 print(f'Os 4 ULTIMOS são: {times[-4:]}')
-
+print(f'Classificação por ordem ALFABÉTICA:{sorted(times)}')
+print('O time Chapecoense está na {}{}'.format(times.index('Chapecoense')+1, 'ª'))
