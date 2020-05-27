a456fa3af0f43477e65ae5120bda582578cef3e2
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-26 22:29:26 -0300

    Times prontos

---
+++ desafio073.py
@@ -0,0 +1,7 @@
+times = ('Palmeiras', 'Flamengo', 'Internacional', 'Grêmio', 'São-Paulo', 'Atlético-MG', 'Atlético-PR', 'Cruzeiro',
+         'Botafogo', 'Santos', 'Bahia', 'Fluminense', 'Corinthians', 'Chapecoense', 'Ceará', 'Vasco', 'América-MG',
+         'Sport', 'Vitória', 'Paraná')
+print(f'Classificação do Brasileirão 2018: {times}')
+print(f'Os 5 PRIMEIROS são: {times[:5]}')
+print(f'Os 4 ULTIMOS são: {times[-4:]}')
+
