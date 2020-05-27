9068205658af0cac6733d2d952baa4e8a8a8ae13
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-05-26 22:51:17 -0300

    Resolvido

--- desafio073.py
+++ desafio073.py
@@ -1,8 +1,8 @@
 times = ('Palmeiras', 'Flamengo', 'Internacional', 'Grêmio', 'São-Paulo', 'Atlético-MG', 'Atlético-PR', 'Cruzeiro',
          'Botafogo', 'Santos', 'Bahia', 'Fluminense', 'Corinthians', 'Chapecoense', 'Ceará', 'Vasco', 'América-MG',
          'Sport', 'Vitória', 'Paraná')
-print(f'Classificação do Brasileirão 2018: {times}')
-print(f'Os 5 PRIMEIROS são: {times[:5]}')
-print(f'Os 4 ULTIMOS são: {times[-4:]}')
-print(f'Classificação por ordem ALFABÉTICA:{sorted(times)}')
-print('O time Chapecoense está na {}{}'.format(times.index('Chapecoense')+1, 'ª'))
+print(f'\033[33mClassificação do Brasileirão 2018: {times}')
+print(f'\033[34mOs 5 PRIMEIROS são: {times[:5]}')
+print(f'\033[31mOs 4 ULTIMOS são: {times[-4:]}')
+print(f'\033[32mClassificação por ordem ALFABÉTICA:{sorted(times)}')
+print('\033[35mO time Chapecoense está na {}{}'.format(times.index('Chapecoense')+1, 'ª'))
