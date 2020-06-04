5a7eecbdc5f27046c7a3f18d3d5362237926006d
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-06-04 13:53:39 -0300

    Update desafio075.py

--- desafio075.py
+++ desafio075.py
@@ -9,7 +9,7 @@ if 3 not in v:
     print('O número 3 não foi digitado e não está em nenhuma posição.')
 else:
     print(f'O número 3 está na {v.index(3)+1}ª posição.')
-print('Os valores pares digitados foram', end= ' ')
+print('Os valores pares digitados foram os números', end= ' ')
 for par in v:
   if  par % 2 == 0:
     print(par, end=' ')
