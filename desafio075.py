8499e3f5c602f1e36815556369af8df12bff5fb8
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-06-04 14:12:32 -0300

    Resolvido

--- desafio075.py
+++ desafio075.py
@@ -1,15 +1,14 @@
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-n3 = int(input('Digite mais um número: '))
-n4 = int(input('Digite um último número: '))
-v = (n1, n2, n3, n4)
-print(f'Você digitou os valores {v}')
-print(f'O número 9 aparece {v.count(9)} vezes.')
-if 3 not in v:
+núm = (int(input('Digite um número: ')),
+int(input('Digite outro número: ')),
+int(input('Digite mais um número: ')),
+int(input('Digite um último número: ')))
+print(f'Você digitou os valores {núm}')
+print(f'O número 9 aparece {núm.count(9)} vezes.')
+if 3 not in núm:
     print('O número 3 não foi digitado e não está em nenhuma posição.')
 else:
-    print(f'O número 3 está na {v.index(3)+1}ª posição.')
+    print(f'O número 3 está na {núm.index(3)+1}ª posição.')
 print('Os valores pares digitados foram os números', end= ' ')
-for par in v:
+for par in núm:
   if  par % 2 == 0:
     print(par, end=' ')
