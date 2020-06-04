517acd5241fa98abf6aaf9d7988d785c44a21985
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-06-04 11:08:16 -0300

    adicionando valores na tupla

---
+++ desafio075.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+n3 = int(input('Digite mais um número: '))
+n4 = int(input('Digite um último número: '))
+v = (n1, n2, n3, n4)
+print('Você digitou os valores {}'.format(v))
