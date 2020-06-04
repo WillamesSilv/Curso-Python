c68160c0d4ce4a41e5d054d095d889b357126095
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-06-04 11:56:32 -0300

    Update desafio075.py

--- desafio075.py
+++ desafio075.py
@@ -4,3 +4,8 @@ n3 = int(input('Digite mais um número: '))
 n4 = int(input('Digite um último número: '))
 v = (n1, n2, n3, n4)
 print('Você digitou os valores {}'.format(v))
+print(f'O número 9 aparece {v.count(9)} vezes.')
+if 3 not in v:
+    print('O número 3 não foi digitado e não está em nenhuma posição.')
+else:
+    print(f'O número 3 está na posição {v.index(3)+1}ª.')
